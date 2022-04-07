428405ec703f6d951de114d1f98a6209c9a8ce77
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-04-07 13:03:37 -0400

    update reservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -90,18 +90,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
+  const queryStringReservations = `SELECT *
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  JOIN users ON users.id = property_reviews.guest_id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.title, properties.cost_per_night
+  ORDER BY start_date
+  LIMIT 10;
+  `
+  const values = guest_id;
   return pool
-    .query(`SELECT reservations.*
-FROM reservations
-JOIN properties ON reservations.property_id = properties.id
-JOIN property_reviews ON property_reviews.property_id = properties.id
-JOIN users ON users.id = property_reviews.guest_id
-WHERE guest.id = 1
-GROUP BY reservations.id, properties.title, properties.cost_per_night
-ORDER BY start_date
-`, limit)
-.then((result) => {
-  // console.log(result.rows);
+  .query(queryStringReservations, [values])
+  .then((result) => {
   return result.rows;
 })
   .catch((err) => {
